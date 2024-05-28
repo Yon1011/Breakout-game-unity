374b39c1eb0ee690e673e9b323e373f16a784b20
Duong Vo Ngoc Thai <duong.vongocthai@gameloft.com>
2024-05-28 16:21:05 +0700

    finish the prototype

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -11,6 +11,7 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
+    public Text BestScoreText;
     public GameObject GameOverText;
     
     private bool m_Started = false;
@@ -36,6 +37,7 @@ public class MainManager : MonoBehaviour
                 brick.onDestroyed.AddListener(AddPoint);
             }
         }
+        BestScoreText.text = $"Best Score : {GameController.Instance.Data.name} : {GameController.Instance.Data.score}";
     }
 
     private void Update()
@@ -72,5 +74,6 @@ public class MainManager : MonoBehaviour
     {
         m_GameOver = true;
         GameOverText.SetActive(true);
+        GameController.Instance.SetHighestScore(m_Points);
     }
 }
